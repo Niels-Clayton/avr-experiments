8a84f2ca78c401194221d655cb6b0d1d0847b7b3
Niels-Clayton <nielsdanielclayton@gmail.com>
2020-12-07 22:58:32 +1300

    Updated blink to use IO as an enable switch

--- blink/main.c
+++ blink/main.c
@@ -1,16 +1,22 @@
 #define F_CPU 16000000UL
-#define LED_PIN 1           // Blink port B pin 0
+#define LED_PIN PORTB1           // Blink port B pin 0
+#define LED_ENABLE PORTB0
 
 #include "blink.h"
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main()
-{
-    DDRB |= (1 << LED_PIN);
-    while(1)
-    {
+int main(void) {
+    DDRB |= (1 << LED_PIN); 	// Set port B pin 1 as output
+
+    DDRB &= ~(1 << LED_ENABLE); // Set port B pin 0 as input
+    PORTB |= (1 << LED_ENABLE); // Enable port B pin 0 internal 50k pullup
+    
+    while(1) {
+	if(PINB & (1 << LED_ENABLE)) { // If port B pin 0 is high, toggle the LED pin
         toggle(&PORTB, LED_PIN);
-        _delay_ms(1000);
+        _delay_ms(100);
+	}
+	else PORTB &= ~(1 << LED_PIN); // Else set LED pin low
     }
 }
